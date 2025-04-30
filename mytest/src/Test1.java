f8aa395d06f94b82578e4ed7d848c787381c92ea
nastya1989db <nastenkaya1989@gmail.com>
2025-04-30 19:27:52 +0300

    mytest&&mytest2 added to GIT

---
+++ mytest/src/Test1.java
@@ -0,0 +1,17 @@
+public class Test1 {
+            public void sayTodayDate() {
+                System.out.println ("13.04.2025");}
+            public void sayHello () {
+                    System.out.println("Hello Yuriy");
+                    System.out.println("Hello Arina");
+                    System.out.println("Hello people");
+                }
+
+                }
+
+
+
+
+
+
+
---
+++ mytest2/src/Test2.java
@@ -0,0 +1,15 @@
+
+public class Test2 {
+    public static void main(String[] args) {
+        // Создаем объект класса Test1
+        Test1 test1 = new Test1();
+
+        // Вызываем метод sayHello() and sayTodayDate();
+        test1.sayHello();
+        test1.sayTodayDate();
+    }
+}
+
+
+
+
